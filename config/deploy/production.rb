c759c474aba37e6f207832939086153e9acb3b25
Sean Perryman <se@nperryman.com>
2014-09-01 12:45:58 -0700

    Post cap install

---
+++ config/deploy/production.rb
@@ -0,0 +1,18 @@
+# Define roles, user and IP address of deployment server
+# role :name, %{[user]@[IP adde.]}
+role :app, %w{deployer@104.131.128.127}
+role :web, %w{deployer@104.131.128.127}
+role :db,  %w{deployer@104.131.128.127}
+
+# Define server(s)
+server '104.131.128.127', user: 'deployer', roles: %w{web}
+
+# SSH Options
+# See the example commented out section in the file
+# for more options.
+set :ssh_options, {
+    forward_agent: false,
+    auth_methods: %w(password),
+    password: 'Fuckyou1youfuck!',
+    user: 'deployer',
+}
