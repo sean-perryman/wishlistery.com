214484625d15f1e1b8016943eaf3f4f70391ea41
Sean Perryman <se@nperryman.com>
2014-09-01 14:16:28 -0700

    making changes

--- Gemfile
+++ Gemfile
@@ -23,27 +23,18 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 
-# Mail Form
-gem 'mail_form'
-
 # Use simple form
 gem 'simple_form'
 
-# TinyMCE (Rich Text Editor)
-#gem 'tinymce-rails'
-
-#Carrierwave to compliment Cloundinary
-#gem 'carrierwave'
-
-#Cloudinary for image management
-#gem 'cloudinary'
-
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
+
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
@@ -52,11 +43,6 @@ gem 'spring',        group: :development
 
 # Text searching
 gem 'mysql2'
-gem 'thinking-sphinx'
-gem 'flying-sphinx',	group: :production
-
-# Per Heroku recommendation
-gem 'rails_12factor',	group: :production
 
 # New Relic Analytics
 gem 'newrelic_rpm'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,44 +1,8 @@
-# Define the name of the application
 set :application, 'wishlistery.com'
-
-# Define where can Capistrano access the source repository
-# set :repo_url, 'https://github.com/[user name]/[application name].git'
 set :scm, :git
 set :repo_url, 'https://github.com/sean-perryman/wishlistery.com.git'
-
-# Define where to put your application code
 set :deploy_to, "/home/deployer/apps/wishlistery.com"
-
 set :pty, true
-
 set :format, :pretty
-
-# Set the post-deployment instructions here.
-# Once the deployment is complete, Capistrano
-# will begin performing them as described.
-# To learn more about creating tasks,
-# check out:
-# http://capistranorb.com/
-
-# namespace: deploy do
-
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # Your restart mechanism here, for example:
-#       execute :touch, release_path.join('tmp/restart.txt')
-#     end
-#   end
-
-#   after :publishing, :restart
-
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
-
-# end
+set :rails_env, "production"
+set :user, "deployer"
